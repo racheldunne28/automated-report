886c184ae08bb4bd59e1de037e0392eacce8c4d3
Rachel Dunne <rachel.dunne@cabinetoffice.gov.uk>
2021-12-23 18:39:56 +0000

    Feat: just ignore relative import line

--- reports/report_example.py
+++ reports/report_example.py
@@ -11,7 +11,7 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 parent_dir_path = os.path.abspath(os.path.join(dir_path, os.pardir))
 sys.path.insert(0, parent_dir_path)
 
-from reports.assets import EXAMPLE_FIGURE
+from reports.assets import EXAMPLE_FIGURE # noqa
 
 CHARTS = {
     "example_figure": EXAMPLE_FIGURE,
