7f724be7a55d1257d16997fdc863ed2303bd1a1b
Rachel Dunne <rachel.dunne@cabinetoffice.gov.uk>
2021-12-23 18:23:48 +0000

    Feat: try to get imports working for tests

--- tests/test_report_example.py
+++ tests/test_report_example.py
@@ -1,10 +1,10 @@
 import os, sys
 
-from reports.report_example import get_data_dict
-
 dir_path = os.path.dirname(os.path.realpath(__file__))
 parent_dir_path = os.path.abspath(os.path.join(dir_path, os.pardir))
 sys.path.insert(0, parent_dir_path)
+
+from reports.report_example import get_data_dict
 
 def test_get_data_dict():
     dd = get_data_dict()
