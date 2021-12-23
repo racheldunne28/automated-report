2cf5a78cd08af411aab2dafebf95fe49af00292a
Rachel Dunne <rachel.dunne@cabinetoffice.gov.uk>
2021-12-23 18:40:40 +0000

    blacken

--- tests/test_report_example.py
+++ tests/test_report_example.py
@@ -5,7 +5,7 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 parent_dir_path = os.path.abspath(os.path.join(dir_path, os.pardir))
 sys.path.insert(0, parent_dir_path)
 
-from reports.report_example import get_data_dict # noqa
+from reports.report_example import get_data_dict  # noqa
 
 
 def test_get_data_dict():
